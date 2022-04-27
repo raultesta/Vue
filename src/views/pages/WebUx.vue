85d9dbfb12df73ea4a4015f4920018adae8b6212
Raúl Testa <raultesta@MacBook-Pro-de-Raul.local>
2022-04-27 07:03:14 +0100

    Adjusted the top margins of the WEBUX page containers and created the SPEAK website section of that page

--- src/views/pages/WebUx.vue
+++ src/views/pages/WebUx.vue
@@ -1,9 +1,17 @@
 <template>
     <page wrapperClass="page-design">
       <b-row>
-        <b-col class="d-flex flex-column align-items-center">
+        <b-col class="d-flex flex-column align-items-center col" style="margin-bottom: 100px;">
           <h2>Web Design, UX/UI & Web Development</h2>
           <p>Nowadays my work has its focus on web and UX/UI design and also on web development </p>
+        </b-col>
+      </b-row>
+      <hexagon-grid :hexagons="hexagons" size="large"/>
+          <b-row>
+        <b-col class="d-flex flex-column align-items-center col" style="margin-top: 280px; margin-bottom: 100px;
+">
+          <h3>WEBdesign - SPEAK website</h3>
+          <p>SPEAK aims at solving the migrant social integration problem. Its webs solution had to be easy to use and to enable class enrollment</p>
         </b-col>
       </b-row>
       <hexagon-grid :hexagons="hexagons" size="large"/>
